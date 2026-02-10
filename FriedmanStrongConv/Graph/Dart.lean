fcfa8189102db4c2b922c9dca8b33282d8aa18cb
Frederick J Nash <fjn1g13@southamptonalumni.ac.uk>
2026-02-10 20:51:14 +0000

    WIP

--- FriedmanStrongConv/Graph/Dart.lean
+++ FriedmanStrongConv/Graph/Dart.lean
@@ -93,11 +93,123 @@ lemma dart_of_edge (e : E(G)) : ∃ d : G.Dart, d.edge = e := by
     orienOfEq := (fun _ => true)
   }
 
+lemma non_loop_orienOfEq {d : G.Dart} (h : d.fst ≠ d.snd) : (d.orienOfEq ≍ (fun (_:False) => true)) := by
+  apply heq_of_eqRec_eq
+  . ext hn
+    exact False.elim hn
+  . rw [eq_false h]
+
+lemma loop_orienOfEq_cases {d : G.Dart} (h : d.fst = d.snd) : (d.orienOfEq ≍ (fun (_:True) => true)) ∨ (d.orienOfEq ≍ (fun (_:True) => false)) := by
+  by_cases cc : d.orienOfEq h
+  . left
+    apply heq_of_eqRec_eq
+    . ext k
+      rw [<- cc]
+      apply congr_heq
+      . exact cast_heq ?_ d.orienOfEq -- I don't understand the hole here: if I try to fill it, then checking fails
+      . exact proof_irrel_heq k h
+    . rw [eq_true h]
+  . right
+    rw [Bool.bool_iff_false] at cc
+    apply heq_of_eqRec_eq
+    . ext k
+      rw [<- cc]
+      apply congr_heq
+      . exact cast_heq ?_ d.orienOfEq -- I don't understand the hole here: if I try to fill it, then checking fails
+      . exact proof_irrel_heq k h
+    . rw [eq_true h]
+
+lemma isLoop_reverse_of_isLoop {d : G.Dart} (h : d.fst = d.snd) : d.reverse.fst = d.reverse.snd := h.symm
+
+-- lemma reverse_loop_orienOfEq {d : G.Dart} (h : d.fst = d.snd) {o : Bool} (ho : d.orienOfEq ≍ (fun (_:True) => o)) : (d.reverse.orienOfEq ≍ (fun (_:True) => !o)) := by
+--   apply heq_of_eqRec_eq
+--   . ext k
+--     unfold Dart.reverse
+
+--   . rw [isLoop_reverse_of_isLoop h]
+--     rw [eq_true (Eq.refl d.reverse.snd)]
+
+lemma isLoop_of_same_edge_of_isLoop {d₁ d₂ : G.Dart} (hedge : d₁.edge = d₂.edge) (h₁ : d₁.fst = d₁.snd) : d₂.fst = d₂.snd := by
+  rcases IsLink.eq_and_eq_or_eq_and_eq d₁.isLink (hedge ▸ d₂.isLink) with ⟨hff, hss⟩ | ⟨hfs, hsf⟩
+  . rw [<- hff, <- hss]
+    exact h₁
+  . rw [<- hfs, <- hsf]
+    exact h₁.symm
+
+lemma isNotLoop_of_same_edge_of_isNotLoop {d₁ d₂ : G.Dart} (hedge : d₁.edge = d₂.edge) (h₁ : d₁.fst ≠ d₁.snd) : d₂.fst ≠ d₂.snd := by
+  rcases IsLink.eq_and_eq_or_eq_and_eq d₁.isLink (hedge ▸ d₂.isLink) with ⟨hff, hss⟩ | ⟨hfs, hsf⟩
+  . rw [<- hff, <- hss]
+    exact h₁
+  . rw [<- hfs, <- hsf]
+    exact h₁.symm
+
+lemma reverse_orienOfEq_eq_of_ne_of_same_edge {d₁ d₂ : G.Dart} (h : d₁ ≠ d₂) (hedge : d₁.edge = d₂.edge) : d₁.orienOfEq ≍ d₂.reverse.orienOfEq := by
+  by_cases h₁ : d₁.fst = d₁.snd
+  . have h₂ := isLoop_of_same_edge_of_isLoop hedge h₁
+    have hr₂ := isLoop_reverse_of_isLoop h₂
+    rcases loop_orienOfEq_cases h₁ with t₁ | f₁ <;> rcases loop_orienOfEq_cases hr₂ with tr₂ | fr₂
+    . exact HEq.trans t₁ tr₂.symm
+    . -- in theory we can provide that the orienOfEq are the same, and so d1 = d2, and thus a contraduction, but that's going to be a lengthly nightmare
+      admit
+    . admit
+    . exact HEq.trans f₁ fr₂.symm
+  . have h₂ := isNotLoop_of_same_edge_of_isNotLoop hedge h₁
+    apply HEq.trans
+    . exact non_loop_orienOfEq h₁
+    . apply HEq.symm
+      exact non_loop_orienOfEq h₂.symm
+
+
+-- lemma loop_orienOfEq_reverse_heq {d₁ d₂ : G.Dart} (h₁ : d₁.fst = d₁.snd) (h₂ : d₂.fst = d₂.snd) : d₁.orienOfEq ≍ d₂.orienOfEq ∨ d₁.orienOfEq ≍ d₂.reverse.orienOfEq := by
+--   by_cases eq : d₁ = d₂
+--   . left
+--     rw [eq]
+--   . right
+--     apply heq_of_eqRec_eq
+--     . ext ht
+
+--       admit
+--     . rw [Dart.fst_of_reverse, Dart.snd_of_reverse]
+--       rw [h₁, h₂]
+--       rw [eq_self _, eq_self _]
+
+-- lemma loop_orienOfEq_heq_or_reverse_heq {d₁ d₂ : G.Dart} (h₁ : d₁.fst = d₁.snd) (h₂ : d₂.fst = d₂.snd) : d₁.orienOfEq ≍ d₂.orienOfEq ∨ d₁.orienOfEq ≍ d₂.reverse.orienOfEq := by
+--   apply?
+--   apply heq_of_eqRec_eq
+--   . ext ht
+--     rw [Dart.fst_of_reverse, Dart.snd_of_reverse] at ht
+
+
+--     admit
+--   . rw [Dart.fst_of_reverse, Dart.snd_of_reverse]
+--     rw [h₁, h₂]
+--     rw [eq_self _, eq_self _]
+
+-- lemma reverse_loop_orienOfEq_eq {d₁ d₂ : G.Dart} (h : d₁.edge = d₂.edge) (h₁ : d₁.fst = d₁.snd) (h₂ : d₂.fst = d₂.snd) : d₁.orienOfEq ≍ d₂.reverse.orienOfEq := by
+--   apply heq_of_eqRec_eq
+--   . ext ht
+--     rw [Dart.fst_of_reverse, Dart.snd_of_reverse] at ht
+
+
+--     admit
+--   . rw [Dart.fst_of_reverse, Dart.snd_of_reverse]
+--     rw [h₁, h₂]
+--     rw [eq_self _, eq_self _]
+
 lemma non_loop_orienOfEq_eq {d₁ d₂ : G.Dart} (h₁ : d₁.fst ≠ d₁.snd) (h₂ : d₂.fst ≠ d₂.snd) : d₁.orienOfEq ≍ d₂.orienOfEq := by
   apply heq_of_eqRec_eq
   . ext hn
     contradiction
   . rw [eq_false h₁, eq_false h₂]
+
+lemma non_loop_orienOfEq_reverse_eq {d : G.Dart} (h : d.fst ≠ d.snd) : d.orienOfEq ≍ d.reverse.orienOfEq := by
+  apply heq_of_eqRec_eq
+  . ext hn
+    rw [Dart.fst_of_reverse, Dart.snd_of_reverse] at hn
+    exfalso
+    exact h hn.symm
+  . rw [Dart.fst_of_reverse, Dart.snd_of_reverse]
+    rw [eq_false h, eq_false h.symm]
 
 /- Two darts are equal or reverse of one another if they have the same edge. -/
 lemma edge_dart_eq {d₁ d₂ : G.Dart} (h : d₁.edge = d₂.edge) : d₁ = d₂ ∨ d₁ = d₂.reverse := by
@@ -124,7 +236,7 @@ lemma edge_dart_eq {d₁ d₂ : G.Dart} (h : d₁.edge = d₂.edge) : d₁ = d�
       . exfalso
         apply eq
         apply Dart.ext <;> try assumption
-        have isLoop₂ := (hff ▸ hss ▸ isLoop) -- for some reason I can't inline this... not that I _want_ to inline it, but why can't I?!
+        have isLoop₂ := (hff ▸ hss ▸ isLoop)
         exact non_loop_orienOfEq_eq isLoop isLoop₂
       . apply Dart.ext <;> try assumption
         have isLoop₂ := (hfs ▸ hsf ▸ isLoop) -- for some reason I can't inline this... not that I _want_ to inline it, but why can't I?!
